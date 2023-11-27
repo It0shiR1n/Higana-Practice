e078c77f505aac04583d2e98b35e729bd28b66a8
Itoshi Rin <86209431+It0shiR1n@users.noreply.github.com>
2023-11-27 00:44:26 +0000

    Add files via upload

---
+++ hiraganaPractice.py
@@ -0,0 +1,61 @@
+import random
+import time
+import os
+import sys
+
+
+def clear():
+    if sys.platform == "win32":
+        os.system("cls")
+
+    elif sys.platform == "linux" or sys.platform == "linux2":
+        os.system("clear")
+
+
+HiraganaAndSoundsDict = {
+    'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o',
+    'か': 'ka', 'き': 'ki', 'く': 'ku', 'け': 'ke', 'こ': 'ko',
+    'さ': 'sa', 'し': 'shi', 'す': 'su', 'せ': 'se', 'そ': 'so',
+    'た': 'ta', 'ち': 'chi', 'つ': 'tsu', 'て': 'te', 'と': 'to',
+    'な': 'na', 'に': 'ni', 'ぬ': 'nu', 'ね': 'ne', 'の': 'no',
+    'は': 'ha', 'ひ': 'hi', 'ふ': 'fu', 'へ': 'he', 'ほ': 'ho',
+    'ま': 'ma', 'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo',
+    'や': 'ya', 'ゆ': 'yu', 'よ': 'yo',
+    'ら': 'ra', 'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro',
+    'わ': 'wa', 'を': 'wo',
+    'ん': 'n'
+}
+
+
+HiraganaList = [
+    'あ', 'い', 'う', 'え', 'お',
+    'か', 'き', 'く', 'け', 'こ',
+    'さ', 'し', 'す', 'せ', 'そ',
+    'た', 'ち', 'つ', 'て', 'と',
+    'な', 'に', 'ぬ', 'ね', 'の',
+    'は', 'ひ', 'ふ', 'へ', 'ほ',
+    'ま', 'み', 'む', 'め', 'も',
+    'や', 'ゆ', 'よ',
+    'ら', 'り', 'る', 'れ', 'ろ',
+    'わ', 'を',
+    'ん',
+
+]
+
+
+
+while True:
+    choice = random.choice(HiraganaList) 
+
+    print(choice) 
+    answer = input("Who sound the japanese hiragana letter does ? ")
+
+    if answer == HiraganaAndSoundsDict[choice]:
+        print("[+] Yep, you right !")
+        time.sleep(3)
+        clear()
+
+    else:
+        print(f"[-] Nop, the sound of the simbol {choice} is: {HiraganaAndSoundsDict[choice]}")
+        time.sleep(3)
+        clear()
